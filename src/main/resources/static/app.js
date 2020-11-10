f0c8b1b01c9d9ecd1493fbc791e0a0c43775576c
farrukh_ahmed <farrukh.ahmed@venturedive.com>
2020-11-10 19:17:05 +0500

    remove cred

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDmiIQJVXO4FYw43OmJtPCQKVp5QRRuz80",
-    authDomain: "fir-push-notification-88ac4.firebaseapp.com",
-    databaseURL: "https://fir-push-notification-88ac4.firebaseio.com",
-    projectId: "fir-push-notification-88ac4",
-    storageBucket: "fir-push-notification-88ac4.appspot.com",
-    messagingSenderId: "77731529146",
-    appId: "1:77731529146:web:8ead6275a5d1c554eedd83"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/7.15.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/7.15.1/firebase-messaging.js");
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDmiIQJVXO4FYw43OmJtPCQKVp5QRRuz80",
-    authDomain: "fir-push-notification-88ac4.firebaseapp.com",
-    databaseURL: "https://fir-push-notification-88ac4.firebaseio.com",
-    projectId: "fir-push-notification-88ac4",
-    storageBucket: "fir-push-notification-88ac4.appspot.com",
-    messagingSenderId: "77731529146",
-    appId: "1:77731529146:web:8ead6275a5d1c554eedd83"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
